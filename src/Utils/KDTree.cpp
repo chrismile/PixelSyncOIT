4bc0d6a5f6c765088d14f78f742ed2260ed91d22
Christoph Neuhauser <c.a.neuhauser@gmail.com>
2019-12-29 19:36:49 +0100

    Extended the k-d-tree data structure.

--- src/Utils/KDTree.cpp
+++ src/Utils/KDTree.cpp
@@ -1,79 +1,164 @@
-//
-// Created by christoph on 28.08.18.
-//
+/*
+ * BSD 2-Clause License
+ *
+ * Copyright (c) 2019, Christoph Neuhauser
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include "KDTree.hpp"
 
-bool Rectangle::contains(const glm::vec3 &pt) const
-{
+bool AxisAlignedBox::contains(const glm::vec3 &pt) const {
     if (pt.x >= min.x && pt.y >= min.y && pt.z >= min.z
         && pt.x <= max.x && pt.y <= max.y && pt.z <= max.z)
         return true;
     return false;
 }
 
-void KDTree::build(const std::vector<Point*> &points)
-{
+KDTree::KDTree() {
+    root = nullptr;
+}
+
+KDTree::~KDTree() {
+    _deleteAllData();
+}
+
+void KDTree::_deleteAllData() {
+    root = nullptr;
+    for (KDNode *node : nodes) {
+        delete node;
+    }
+    nodes.clear();
+}
+
+void KDTree::build(const std::vector<IndexedPoint*> &points) {
     root = _build(points, 0);
 }
 
-std::shared_ptr<KDNode> KDTree::_build(std::vector<Point*> points, int depth)
-{
+KDNode *KDTree::_build(std::vector<IndexedPoint*> points, int depth) {
+    _deleteAllData();
     const int k = 3; // Number of dimensions
 
     if (points.size() == 0) {
-        return std::shared_ptr<KDNode>();
+        return nullptr;
     }
 
     int axis = depth % k;
-    std::sort(points.begin(), points.end(), [axis](Point *a, Point *b) {
+    std::sort(points.begin(), points.end(), [axis](IndexedPoint *a, IndexedPoint *b) {
         if (axis == 0) return a->position.x < b->position.x;
         if (axis == 1) return a->position.y < b->position.y;
         return a->position.z < b->position.z;
     });
     size_t medianIndex = points.size() / 2;
-    std::vector<Point*> leftPoints = std::vector<Point*>(points.begin(), points.begin() + medianIndex);
-    std::vector<Point*> rightPoints = std::vector<Point*>(points.begin() + medianIndex + 1, points.end());
+    std::vector<IndexedPoint*> leftPoints = std::vector<IndexedPoint*>(points.begin(), points.begin() + medianIndex);
+    std::vector<IndexedPoint*> rightPoints = std::vector<IndexedPoint*>(points.begin() + medianIndex + 1, points.end());
 
-    std::shared_ptr<KDNode> node = std::shared_ptr<KDNode>(new KDNode);
+    KDNode *node = new KDNode;
     node->axis = axis;
-    node->location = points.at(medianIndex);
+    node->point = points.at(medianIndex);
     node->left = _build(leftPoints, depth + 1);
     node->right = _build(rightPoints, depth + 1);
+    nodes.push_back(node);
     return node;
 }
 
+void KDTree::addPoint(IndexedPoint *point) {
+    root = _addPoint(root, 0, point);
+}
 
-std::vector<Point*> KDTree::findPointsInRectangle(const Rectangle &rect)
-{
-    std::vector<Point*> points;
-    _findPointsInRectangle(rect, points, root);
+KDNode *KDTree::_addPoint(KDNode *node, int depth, IndexedPoint *point) {
+    const int k = 3; // Number of dimensions
+    const int axis = depth % k;
+
+    if (node == nullptr) {
+        KDNode *node = new KDNode;
+        node->axis = axis;
+        node->point = point;
+        nodes.push_back(node);
+        return node;
+    }
+
+    if ((axis == 0 && point->position.x < node->point->position.x)
+        || (axis == 1 && point->position.y < node->point->position.y)
+        || (axis == 2 && point->position.z < node->point->position.z)) {
+        node->left = _addPoint(node->left, depth + 1, point);
+    } else {
+        node->right = _addPoint(node->right, depth + 1, point);
+    }
+    return node;
+}
+
+std::vector<IndexedPoint*> KDTree::findPointsInAxisAlignedBox(const AxisAlignedBox &box) {
+    std::vector<IndexedPoint*> points;
+    _findPointsInAxisAlignedBox(box, points, root);
     return points;
 }
 
-void KDTree::_findPointsInRectangle(const Rectangle &rect, std::vector<Point*> &points, std::shared_ptr<KDNode> node)
-{
-    if (rect.contains(node->location->position)) {
-        points.push_back(node->location);
+std::vector<IndexedPoint*> KDTree::findAllPointsWithinRadius(IndexedPoint *centerPoint, float radius) {
+    std::vector<IndexedPoint*> pointsWithDistance;
+    std::vector<IndexedPoint*> pointsInRect;
+
+    // First, find all points within the bounding box containing the search circle.
+    AxisAlignedBox box;
+    box.min = centerPoint->position - glm::vec3(radius, radius, radius);
+    box.max = centerPoint->position + glm::vec3(radius, radius, radius);
+    _findPointsInAxisAlignedBox(box, pointsInRect, root);
+
+    // Now, filter all points out that are not within the search radius.
+    float squaredRadius = radius*radius;
+    glm::vec3 differenceVector;
+    for (IndexedPoint *point : pointsInRect) {
+        differenceVector = centerPoint->position - centerPoint->position;
+        if (differenceVector.x*differenceVector.x + differenceVector.y*differenceVector.y
+                + differenceVector.z*differenceVector.z <= squaredRadius) {
+            pointsWithDistance.push_back(point);
+        }
     }
 
-    if ((node->axis == 0 && rect.min.x <= node->location->position.x)
-        || (node->axis == 1 && rect.min.y <= node->location->position.y)
-        || (node->axis == 2 && rect.min.z <= node->location->position.z)) {
-        _findPointsInRectangle(rect, points, node->left);
+    return pointsWithDistance;
+}
+
+void KDTree::_findPointsInAxisAlignedBox(const AxisAlignedBox &rect, std::vector<IndexedPoint*> &points, KDNode *node) {
+    if (rect.contains(node->point->position)) {
+        points.push_back(node->point);
     }
-    if ((node->axis == 0 && rect.max.x >= node->location->position.x)
-        || (node->axis == 1 && rect.max.y >= node->location->position.y)
-        || (node->axis == 2 && rect.max.z >= node->location->position.z)) {
-        _findPointsInRectangle(rect, points, node->right);
+
+    if ((node->axis == 0 && rect.min.x <= node->point->position.x)
+        || (node->axis == 1 && rect.min.y <= node->point->position.y)
+        || (node->axis == 2 && rect.min.z <= node->point->position.z)) {
+        _findPointsInAxisAlignedBox(rect, points, node->left);
+    }
+    if ((node->axis == 0 && rect.max.x >= node->point->position.x)
+        || (node->axis == 1 && rect.max.y >= node->point->position.y)
+        || (node->axis == 2 && rect.max.z >= node->point->position.z)) {
+        _findPointsInAxisAlignedBox(rect, points, node->right);
     }
 };
 
 
-Point *KDTree::findCloseIndexedPoint(Point *centerPoint, float maxDistance)
-{
+IndexedPoint *KDTree::findClosestIndexedPoint(IndexedPoint *centerPoint, float maxDistance) {
     // Find all points in bounding rectangle and test if their distance is less or equal than searched one
-    Rectangle rect;
+    AxisAlignedBox rect;
     rect.min = glm::vec3(
             centerPoint->position.x - maxDistance,
             centerPoint->position.y - maxDistance,
@@ -83,11 +168,11 @@ Point *KDTree::findCloseIndexedPoint(Point *centerPoint, float maxDistance)
             centerPoint->position.y + maxDistance,
             centerPoint->position.z + maxDistance);
 
-    std::vector<Point*> rectPoints = findPointsInRectangle(rect);
+    std::vector<IndexedPoint*> rectPoints = findPointsInAxisAlignedBox(rect);
 
-    Point *closestPoint = NULL;
+    IndexedPoint *closestPoint = NULL;
     float closestDistance = FLT_MAX;
-    for (Point *point : rectPoints) {
+    for (IndexedPoint *point : rectPoints) {
         float currDistance = (centerPoint->position - point->position).length();
         if (currDistance < closestDistance && currDistance < maxDistance) {
             closestPoint = point;
--- src/Utils/KDTree.hpp
+++ src/Utils/KDTree.hpp
@@ -1,6 +1,30 @@
-//
-// Created by christoph on 28.08.18.
-//
+/*
+ * BSD 2-Clause License
+ *
+ * Copyright (c) 2019, Christoph Neuhauser
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #ifndef PIXELSYNCOIT_KDTREE_HPP
 #define PIXELSYNCOIT_KDTREE_HPP
@@ -10,44 +34,128 @@
 #include <vector>
 #include <glm/glm.hpp>
 
-class Point
+/**
+ * Class representing a 3D point with an additional index attribute.
+ */
+class IndexedPoint
 {
 public:
     glm::vec3 position;
     int index;
 };
 
-class Rectangle
+/**
+ * An axis aligned (bounding) box for search queries in the k-d-tree.
+ */
+class AxisAlignedBox
 {
 public:
+    /// The minimum and maximum coordinate corners of the 3D cuboid.
     glm::vec3 min, max;
+
+    /**
+     * Tests whether the axis aligned box contains a point.
+     * @param pt The point.
+     * @return True if the box contains the point.
+     */
     bool contains(const glm::vec3 &pt) const;
 };
 
+/**
+ * A node in the k-d-tree. It stores in which axis the space is partitioned (x,y,z)
+ * as an index, the position of the node, and its left and right children.
+ */
 class KDNode
 {
 public:
+    KDNode() : axis(0), point(nullptr), left(nullptr), right(nullptr) {}
     int axis;
-    Point *location;
-    std::shared_ptr<KDNode> left;
-    std::shared_ptr<KDNode> right;
+    IndexedPoint *point;
+    KDNode *left;
+    KDNode *right;
 };
 
+/**
+ * The k-d-tree class. Used for searching point sets in space efficiently.
+ * NOTE: The ownership of the memory the IndexedPoint objects lies in the responsibility of the user.
+ */
 class KDTree
 {
-    void build(const std::vector<Point*> &points);
+public:
+    KDTree();
+    ~KDTree();
 
-    /// Used for e.g. giving points that are approx. the same (i.e. low distance)
-    /// the same index.
-    Point *findCloseIndexedPoint(Point *centerPoint, float maxDistance);
-    std::vector<Point*> findPointsInRectangle(const Rectangle &rect);
+    /**
+     * Builds a k-d-tree from the passed point array.
+     * @param points The point array.
+     */
+    void build(const std::vector<IndexedPoint*> &points);
+
+    /**
+     * Adds a point to the k-d-tree.
+     * NOTE: If you want to add more than one point, rather use @see build.
+     * The addPoint method can generate a degenerate tree if the points come in in an unfavorable order.
+     * @param point The point to add to the tree.
+     */
+    void addPoint(IndexedPoint *point);
+
+    /**
+     * Performs an area search in the k-d-tree and returns all points within a certain bounding box.
+     * @param box The bounding box.
+     * @return The points of the k-d-tree inside of the bounding box.
+     */
+    std::vector<IndexedPoint*> findPointsInAxisAlignedBox(const AxisAlignedBox &box);
+
+    /**
+     * Performs an area search in the k-d-tree and returns all points within a certain distance to the center point.
+     * @param centerPoint The center point.
+     * @param radius The search radius.
+     * @return The points of the k-d-tree inside of the search radius.
+     */
+    std::vector<IndexedPoint*> findAllPointsWithinRadius(IndexedPoint *centerPoint, float radius);
+
+    /**
+     * Performs an area search in the k-d-tree and returns the closest point to a certain specified point.
+     * If no point is found within a specified maximum distance, null is returned.
+     * @param {IndexedPoint} centerPoint The point of which to find the closest neighbor (excluding itself).
+     * @param {number} maxDistance The maximum distance the two points may have.
+     * @return {IndexedPoint} The closest neighbor within the maximum distance.
+     */
+    IndexedPoint *findClosestIndexedPoint(IndexedPoint *centerPoint, float maxDistance);
 
 private:
     // Internal implementations
-    std::shared_ptr<KDNode> _build(std::vector<Point*> points, int depth);
-    void _findPointsInRectangle(const Rectangle &rect, std::vector<Point*> &points, std::shared_ptr<KDNode> node);
+    /**
+     * Builds a k-d-tree from the passed point array recursively (for internal use only).
+     * @param points The point array.
+     * @return The parent node of the current sub-tree.
+     */
+    KDNode *_build(std::vector<IndexedPoint*> points, int depth);
 
-    std::shared_ptr<KDNode> root;
+    /**
+     * Adds a point to the k-d-tree (for internal use only).
+     * @param node The current node to process (or null).
+     * @param depth The current depth in the tree.
+     * @param point The point to add to the tree.
+     * @return The node object above or a new leaf node.
+     */
+    KDNode *_addPoint(KDNode *node, int depth, IndexedPoint *point);
+
+    /**
+     * Performs an area search in the k-d-tree and returns all points within a certain bounding box
+     * (for internal use only).
+     * @param box The bounding box.
+     * @param node The current k-d-tree node that is searched.
+     * @param points The points of the k-d-tree inside of the bounding box.
+     */
+    void _findPointsInAxisAlignedBox(const AxisAlignedBox &box, std::vector<IndexedPoint*> &points, KDNode *node);
+
+    /// Root of the tree
+    KDNode *root;
+
+    // For deletion
+    void _deleteAllData();
+    std::vector<KDNode*> nodes;
 };
 
 #endif //PIXELSYNCOIT_KDTREE_HPP
